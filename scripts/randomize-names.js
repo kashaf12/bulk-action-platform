eef1c38e5a29ae4bd5ff8147b91b06cf4039757f
kashaf12 <kashafaahmed@gmail.com>
2025-05-27 22:14:45 +0530

    fix: update input/output file paths and add error handling for missing input file in randomize-names script

--- scripts/randomize-names.js
+++ scripts/randomize-names.js
@@ -1,9 +1,13 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 import { createObjectCsvWriter } from 'csv-writer';
+import path from 'path';
+console.log('Current script path:', process.cwd());
 
-const inputFile = 'scripts/csv/data-1000000.csv';
-const outputFile = 'scripts/csv/updated-data-1000000.csv';
+const inputFile = path.resolve('./seed_data/contact-100000.csv');
+const outputFile = path.resolve('./seed_data/updated-contact-100000.csv');
+
+const max_rows = 100_000;
 
 // Generate random alphanumeric string
 const randomString = (length = 10) =>
@@ -14,13 +18,33 @@ const randomString = (length = 10) =>
 // Read CSV, modify names, write new CSV
 const rows = [];
 
-fs.createReadStream(inputFile)
+if (!fs.existsSync(inputFile)) {
+  console.error(`❌ Input file not found: ${inputFile}`);
+  process.exit(1);
+}
+
+const readStream = fs
+  .createReadStream(inputFile)
   .pipe(csv())
   .on('data', row => {
     row.name = randomString(); // Replace 'name' with random string
     rows.push(row);
+    if (rows.length === max_rows) {
+      readStream.destroy();
+      const headers = Object.keys(rows[0]).map(key => ({ id: key, title: key }));
+      const writer = createObjectCsvWriter({ path: outputFile, header: headers });
+
+      writer
+        .writeRecords(rows)
+        .then(() => console.log(`✅ CSV written to ${outputFile}`))
+        .catch(console.error);
+    }
   })
   .on('end', () => {
+    if (rows.length === 0) {
+      console.error('❌ No data found in CSV.');
+      process.exit(1);
+    }
     const headers = Object.keys(rows[0]).map(key => ({ id: key, title: key }));
     const writer = createObjectCsvWriter({ path: outputFile, header: headers });
 
